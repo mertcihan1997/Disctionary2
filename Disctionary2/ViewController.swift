49396b9998b01c7f48ed1483464cc3ae99773b59
mertcihan1997 <mertcihan1997@gmail.com>
2015-01-18 12:19:14 +0400

    Dictionary
    dictionary

--- Disctionary2/ViewController.swift
+++ Disctionary2/ViewController.swift
@@ -10,6 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var nameText: UITextField!
+      
+    @IBAction func nameButton(sender: AnyObject) {
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
